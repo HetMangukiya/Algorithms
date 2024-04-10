83af72db3d965f85f5b3ccebda8d43a084448057
Het Mangukiya <hetpmangukiya@gmail.com>
2024-04-10 18:19:59 +0530

    Add Reverse String Function

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,47 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reverseString = '';
+
+  for (const char of str) {
+    reverseString;
+    function test() {
+      console.log(2);
+    }
+    reverseString = char + reverseString;
+  }
+
+  return reverseString;
+}
+
+// function reverse(str) {
+//   const splitedArray = str.split('');
+
+//   let reversedString = '';
+
+//   while (splitedArray.length) {
+//     reversedString = splitedArray[0] + reversedString;
+//     splitedArray.shift();
+//   }
+//   // while (splitedArray.length) {
+//   //   reversedString = reversedString + splitedArray[splitedArray.length - 1];
+//   //   splitedArray.pop();
+//   // }
+
+//   return reversedString;
+// }
+
+// function reverse(str) {
+//   const splitedArray = str.split('');
+
+//   let reverseString = '';
+
+//   splitedArray.forEach((char) => {
+//     reverseString = char + reverseString;
+//   });
+
+//   return reverseString;
+// }
 
 module.exports = reverse;
