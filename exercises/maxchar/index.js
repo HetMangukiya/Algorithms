dd1ec57ff8b114ad2a8546c4ed6421ea7f054333
Het Mangukiya <hetpmangukiya@gmail.com>
2024-04-15 13:42:17 +0530

    Add Max Char Function

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  const maxChar = { char: '', num: 0 };
+  for (const char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (const char in charMap) {
+    if (charMap[char] > maxChar.num) {
+      maxChar.char = char;
+      maxChar.num = charMap[char];
+    }
+  }
+
+  return maxChar.char;
+}
+
+maxChar('The quick brown fox jumped over the lazy dog.');
 
 module.exports = maxChar;
